66c2a5987f9eaf25d1163f251b1ef68c39392f10
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-09 00:28:02 +0530

    set up the typescript

---
+++ 01_intro/intro.js
@@ -0,0 +1,4 @@
+// This js file got generated automatically when we tried to run the ts code via tsc intro.ts
+var user = { name: "Ayush", age: 19 };
+console.log("Ayush");
+console.log(user.name);
---
+++ 01_intro/intro.ts
@@ -0,0 +1,4 @@
+// let user = {name: "Ayush", age: 19}; // gives error
+
+console.log("Ayush");
+console.log(user.name);
