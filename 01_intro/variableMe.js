c253b4e59f079ecbd37fcd0d378a63568d00d010
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-09 10:43:08 +0530

    variables in Ts

---
+++ 01_intro/variableMe.js
@@ -0,0 +1,3 @@
+// let variableName: type = value
+var greetings = "Hello Ayush!";
+console.log(greetings);
---
+++ 01_intro/variableMe.ts
@@ -0,0 +1,15 @@
+// let variableName: type = value
+
+let greetings: string = "Hello Ayush!";
+
+// greetings = 6 Throws an error
+// greetings = true also thrwos an error
+
+let myNum: number = 6
+// myNum.toUpperCase() error
+
+greetings.toLowerCase()
+
+console.log(greetings);
+
+export {}
