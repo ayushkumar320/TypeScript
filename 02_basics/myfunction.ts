84b8e0cf4e015451e9977be0a0c431c38a48e33d
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-09 19:02:43 +0530

    update function in TS

--- 02_basics/myfunction.ts
+++ 02_basics/myfunction.ts
@@ -3,6 +3,13 @@ function addTwo(num: number){
   return "hello"  // We can also strict the return type of a function, here it is not so we can return anything here
 }
 
+// To fix the above problem, we can write like this
+
+function addThree(num: number): number {
+  // return "hello" Throws error
+  return num + 3
+}
+
 function getUpper(val: string){
   return val.toUpperCase()
 }
@@ -14,9 +21,18 @@ const loginUser = (name: string, email: string, isPaid: boolean = false) => {}
 getUpper("hello typescript")
 
 let myValue = addTwo(5)
+let anotherVal = addThree(3)
 
 signUpUser("ayush", "ayush@example.com", true)
 
 loginUser("h", "h@h.com") // false by default
+
+function getValue(myVal: number) {
+  if(myVal > 5){
+    return true
+  }
+  return "200 OK"
+}
+
 
 export {}
