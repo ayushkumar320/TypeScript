9f60e6c0de85add17f8f77c4be7bf105ef345c11
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-10 11:28:56 +0530

    explored more things about functions

--- 02_basics/myfunction.ts
+++ 02_basics/myfunction.ts
@@ -1,38 +1,58 @@
-function addTwo(num: number){
+function addTwo(num: number) {
   // return num+2
-  return "hello"  // We can also strict the return type of a function, here it is not so we can return anything here
+  return "hello"; // We can also strict the return type of a function, here it is not so we can return anything here
 }
 
 // To fix the above problem, we can write like this
 
 function addThree(num: number): number {
   // return "hello" Throws error
-  return num + 3
+  return num + 3;
 }
 
-function getUpper(val: string){
-  return val.toUpperCase()
+function getUpper(val: string) {
+  return val.toUpperCase();
 }
 
 function signUpUser(name: string, email: string, isPaid: boolean) {}
 
-const loginUser = (name: string, email: string, isPaid: boolean = false) => {} 
+const loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
-getUpper("hello typescript")
+getUpper("hello typescript");
 
-let myValue = addTwo(5)
-let anotherVal = addThree(3)
+let myValue = addTwo(5);
+let anotherVal = addThree(3);
 
-signUpUser("ayush", "ayush@example.com", true)
+signUpUser("ayush", "ayush@example.com", true);
 
-loginUser("h", "h@h.com") // false by default
+loginUser("h", "h@h.com"); // false by default
 
 function getValue(myVal: number) {
-  if(myVal > 5){
-    return true
+  if (myVal > 5) {
+    return true;
   }
-  return "200 OK"
+  return "200 OK";
 }
 
+const getHello = (name: string): string => {
+  return `Hello ${name}`;
+};
 
-export {}
+const heros = ["thor", "spiderman", "ironman"]
+
+heros.map((hero): string => {
+  return `hero is ${hero}`
+})
+
+
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+  // return 1; Throws error as return type is void
+}
+
+// Never is not same as void, as void means return nothing
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
+}
+
+export {};
